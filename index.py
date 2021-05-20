35e336d491f9b80b5ec291777ebd1d7929ab54b5
lehuy2012 <huylv95@gmail.com>
2021-05-20 17:52:37 +0700

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import logging
 import rds_config
 import pymysql
 #rds settings
-rds_host  = "rds-proxy-test.proxy-cqsrerje9uol.ap-northeast-1.rds.amazonaws.com"
+rds_host  = "rds-proxy-texxxxxxxxxxxxxxxp-northeast-1.rds.amazonaws.com"
 name = rds_config.db_username
 password = rds_config.db_password
 db_name = rds_config.db_name
